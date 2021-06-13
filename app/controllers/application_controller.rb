7c69518b012166649c616d74910056ad4cf5cd12
sota-onozuka <sonta.afcot37@gmail.com>
2021-06-13 19:42:39 +0900

    rubocopのアラートを修正しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,17 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   around_action :switch_locale
 
   def switch_locale(&action)
-  logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
-  locale = extract_locale_from_accept_language_header
-  logger.debug "* Locale set to '#{locale}'"
-  I18n.with_locale(locale, &action)
+    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    locale = extract_locale_from_accept_language_header
+    logger.debug "* Locale set to '#{locale}'"
+    I18n.with_locale(locale, &action)
   end
 
   private
+
   def extract_locale_from_accept_language_header
     request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
   end
