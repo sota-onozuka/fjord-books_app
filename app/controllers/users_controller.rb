4d01009950541e370040d1bf0b23c331b49ca465
sota-onozuka <sonta.afcot37@gmail.com>
2021-06-21 13:18:05 +0900

    testをgitignoreする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@
 
 class UsersController < ApplicationController
   def index
-    puts "うおおおおおおおおおおおおおおおおおおおおおおお"
     @users = User.with_attached_avatar.order(:id).page(params[:page]).per(3)
   end
 
