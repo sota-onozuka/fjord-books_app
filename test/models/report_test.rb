c2ca328ac06f4aa2fe9e411705b586419564f0d9
sota-onozuka <sonta.afcot37@gmail.com>
2021-06-25 16:44:58 +0900

    ReportとUserのインスタンスメソッドのモデルテストの作成

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -1,9 +1,30 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'date'
 
 class ReportTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def editable?(target_user)
+    user == target_user
+  end
+
+  test "#created_on" do
+    me = User.create!(email: 'me@example.com', password: 'password1')
+    she = User.create!(email: 'she@example.com', password: 'password2')
+    report = Report.create!(title: 'testtitle', content: 'testcontent', user_id: me.id)
+    assert_equal report.created_on, Date.today
+  end
+
+  test "#editable?" do
+    me = User.create!(email: 'me@example.com', password: 'password1')
+    she = User.create!(email: 'she@example.com', password: 'password2')
+    report = Report.create!(title: 'testtitle', content: 'testcontent', user_id: me.id)
+
+    assert report.editable?(me)
+    assert_not report.editable?(she)
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,49 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "#name_or_email" do
+    user = User.new(email: 'foo@example.com', name: '')
+    assert_equal 'foo@example.com', user.name_or_email
+
+    user.name = 'Foo Bar'
+    assert_equal 'Foo Bar', user.name_or_email
+  end
+
+  test "#follow" do
+    me = User.create!(email: 'me@example.com', password: 'password1')
+    she = User.create!(email: 'she@example.com', password: 'password2')
+    assert_not me.following?(she)
+    me.follow(she)
+    assert me.following?(she)
+  end
+
+  test "#unfollow" do
+    me = User.create!(email: 'me@example.com', password: 'password1')
+    she = User.create!(email: 'she@example.com', password: 'password2')
+    assert_not me.following?(she)
+    me.follow(she)
+    assert me.following?(she)
+    me.unfollow(she)
+    assert_not me.following?(she)
+  end
+
+  test "#following?" do
+    me = User.create!(email: 'me@example.com', password: 'password1')
+    she = User.create!(email: 'she@example.com', password: 'password2')
+    me.follow(she)
+    assert me.following?(she) #true 正常系？
+    assert_not she.following?(me) #false
+    me.unfollow(she)
+    assert_not me.following?(she) #true 正常系？
+  end
+
+  test "#followed_by?" do
+    me = User.create!(email: 'me@example.com', password: 'password1')
+    she = User.create!(email: 'she@example.com', password: 'password2')
+    me.follow(she)
+    assert she.followed_by?(me) #true 正常系？
+    assert_not me.followed_by?(she) #false
+    me.unfollow(she)
+    assert_not she.followed_by?(me) #true 正常系？
+  end
 end
