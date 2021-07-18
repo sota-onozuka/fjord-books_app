2195bfbcf9299ec34a98a8cb34d8f9e23f3fd2b6
sota-onozuka <sonta.afcot37@gmail.com>
2021-07-18 19:15:25 +0900

    reportsテーブルが削除されたバグを修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_22_083310) do
+ActiveRecord::Schema.define(version: 2021_07_18_100958) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -79,6 +79,16 @@ ActiveRecord::Schema.define(version: 2021_06_22_083310) do
     t.index ["following_id"], name: "index_relationships_on_following_id"
   end
 
+  create_table "reports", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "content", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_reports_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_reports_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -98,4 +108,5 @@ ActiveRecord::Schema.define(version: 2021_06_22_083310) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "users"
+  add_foreign_key "reports", "users"
 end
